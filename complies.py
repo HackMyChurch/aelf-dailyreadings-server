464bbd24de72350c44f8ce444e2a9a799829a30b
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2016-05-31 21:57:47 +0200

    feat: merge benediction + oraison on comlies

--- complies.py
+++ complies.py
@@ -27,16 +27,19 @@ def postprocess(version, variant, data, day, month, year):
     # Insert "Bénédiction" after "Oraison et Bénédiction" and fix it to only mention "Oraison"
     oraison = get_item_by_title(items, u"oraison")
     benediction = get_item_by_title(items, u"bénédiction")
-    html_benediction = u"".join([unicode(l) for l in benediction_aelf.children])
+    html_benediction = u"".join([unicode(l).strip() for l in benediction_aelf.children])
+    html_oraison = benediction.description.string.strip()
 
-    # This is "Oraison et bénédiction" --> split + insert new benediction on dedicated slide
-    # TODO: future versions, keep it "oraison et bénédiction"
+    # This is "Oraison et bénédiction": append bénédiction
     if benediction == oraison or oraison is None:
-        benediction = copy.copy(oraison)
-        oraison.title.string = u"Oraison"
-        benediction.title.string = u"Bénédiction"
-        benediction.description.string = html_benediction
-        oraison.insert_after(benediction)
+        oraison.title.string = u"Oraison et bénédiction"
+
+        if not '<p' in html_benediction:
+            html_benediction = "<p>%s</p>" % html_benediction
+        if not '<p' in html_oraison:
+            html_oraison = "<p>%s</p>" % html_oraison
+
+        benediction.description.string = html_oraison+html_benediction
 
     # All done
     return soup.prettify()
--- tests/test_office_complies.py
+++ tests/test_office_complies.py
@@ -16,8 +16,16 @@ class TestOfficeComplies(TestBase):
         items = self.parseItems(resp.data)
 
         # Validate: Once, before the Intercession
-        self.assertEqual(13, len(items))
+        self.assertEqual(12, len(items))
         self.assertEqual(u"Hymne Salut, Reine des cieux ! Salut, Reine des anges !", items[-1][0])
-        self.assertEqual(u"Bénédiction", items[-2][0])
-        self.assertEqual(u"Oraison", items[-3][0])
+        self.assertEqual(u"Oraison et bénédiction", items[-2][0])
+        self.assertEqual(
+            u"<p>"+
+                u"Seigneur, tandis que nous dormirons en paix, fais germer et grandir jusqu&rsquo;&agrave; la moisson la semence du Royaume des cieux que nous avons jet&eacute;e en terre par le travail de cette journ&eacute;e. Par J&eacute;sus, le Christ, notre Seigneur. Amen."+
+            u"</p>"+
+            u"<p>"+
+                u"Que le Seigneur fasse resplendir sur nous son visage et nous accorde sa grâce. Amen."+
+            u"</p>",
+            items[-2][1]
+        )
 
