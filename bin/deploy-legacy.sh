2380c679d1286dc60bcc79709426d43a29ac3a30
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2017-01-26 22:01:46 +0100

    re-add legacy deploy script

---
+++ bin/deploy-legacy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Get connection details
+eval $(docker-machine env aelf)
+
+# Deploy
+docker build -t aelf-api .
+
+if docker ps --all | grep -q aelf-api
+then
+    docker stop aelf-api
+    docker rm aelf-api
+fi
+docker run --name aelf-api -d -p 4001:4000 --restart always aelf-api
+
+# Follow
+docker logs -f aelf-api
+
