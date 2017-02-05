f40a6e10c354a97c8706a46d1c7c4afef2d0899f
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2017-02-06 00:32:39 +0100

    add beta env to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,27 +4,41 @@ set -e
 
 TAG=$(date +"%Y-%m-%d-%H-%M-%S")
 
+# Determine environment
+case $1 in
+    prod)
+        echo "[INFO] Will deploy in *prod* mode"
+        DOCKER_NAME="aelf-api"
+        DOCKER_PORT=4001
+        ;;
+    *)
+        echo "[INFO] Will deploy in *BETA* mode (to deploy in prod, use --> $0 prod)"
+        DOCKER_NAME="aelf-api-beta"
+        DOCKER_PORT=4002
+        ;;
+esac
+
 # Build
 for machine in $(docker-machine ls --quiet | grep 'prod\.epitre\.co$')
 do
     (
-        echo "[INFO] Starting build of 'aelf-api:$TAG' on $machine"
+        echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine"
         eval $(docker-machine env $machine)
-        docker build --quiet -t "aelf-api:$TAG" .
-        echo "[INFO] Finished build of 'aelf-api:$TAG' on $machine"
+        docker build --quiet -t "${DOCKER_NAME}:${TAG}" .
+        echo "[INFO] Finished build of '${DOCKER_NAME}:${TAG}' on $machine"
     )&
 done
 wait
 
 # Deploy
 eval $(docker-machine env gra-01.prod.epitre.co)
-if docker service inspect aelf-api &>/dev/null
+if docker service inspect ${DOCKER_NAME} &>/dev/null
 then
-    echo "[INFO] Updating service 'aelf-api:$TAG'"
-    docker service update --image aelf-api:$TAG aelf-api
+    echo "[INFO] Updating service '${DOCKER_NAME}:${TAG}:$TAG'"
+    docker service update --image ${DOCKER_NAME}:${TAG} --publish-add ${DOCKER_PORT}:4000 ${DOCKER_NAME}
 else
-    echo "[INFO] Creating service 'aelf-api:$TAG'"
-    docker service create --mode global --restart-condition any --name aelf-api -p 4001:4000 aelf-api:$TAG
+    echo "[INFO] Creating service '${DOCKER_NAME}:${TAG}:$TAG'"
+    docker service create --mode global --restart-condition any --name ${DOCKER_NAME} -p ${DOCKER_PORT}:4000 ${DOCKER_NAME}:${TAG}
 fi
 
 # Follow
