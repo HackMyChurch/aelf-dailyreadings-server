c1023e1a98082eb2ab3cd7079000a101833068e3
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2018-03-17 00:45:28 +0100

    make the server bind on the host rather than routing mesh
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -35,10 +35,10 @@ eval $(docker-machine env gra-01.prod.epitre.co)
 if docker service inspect ${DOCKER_NAME} &>/dev/null
 then
     echo "[INFO] Updating service '${DOCKER_NAME}:${TAG}:$TAG'"
-    docker service update --image ${DOCKER_NAME}:${TAG} --publish-add ${DOCKER_PORT}:4000 ${DOCKER_NAME}
+    docker service update --image ${DOCKER_NAME}:${TAG} --publish-add published=${DOCKER_PORT},target=4000,mode=host ${DOCKER_NAME}
 else
     echo "[INFO] Creating service '${DOCKER_NAME}:${TAG}:$TAG'"
-    docker service create --mode global --restart-condition any --name ${DOCKER_NAME} -p ${DOCKER_PORT}:4000 ${DOCKER_NAME}:${TAG}
+    docker service create --mode global --restart-condition any --name ${DOCKER_NAME} --publish published=${DOCKER_PORT},target=4000,mode=host ${DOCKER_NAME}:${TAG}
 fi
 
 # Follow
