bfbdf715e48121f2c37a82bf7ab781b45b6a62c8
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2024-04-21 14:13:24 +0200

    Use DNS for discovery rather than manually maintained docker context list
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,11 +16,13 @@ unzip -o "${DICT_ARCHIVE}" "fr-classique.aff" "fr-classique.dic"
 case $1 in
     prod)
         echo "[INFO] Will deploy in *prod* mode"
+        HOSTS="prod.api.app.epitre.co"
         DOCKER_NAME="aelf-api"
         DOCKER_PORT=4001
         ;;
     *)
         echo "[INFO] Will deploy in *BETA* mode (to deploy in prod, use --> $0 prod)"
+        HOSTS="beta.api.app.epitre.co"
         DOCKER_NAME="aelf-api-beta"
         DOCKER_PORT=4002
         ;;
@@ -28,23 +30,24 @@ esac
 
 # Build and deploy
 TAG=$(date +"%Y-%m-%d-%H-%M-%S")
-for machine in $(docker context ls --quiet | grep 'prod\.epitre\.co$' | grep -v '^mon')
+for machine_ip in $(dig +short "${HOSTS}")
 do
+    machine="$(dig +short -x "${machine_ip}")"
     (
-        echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine"
-        docker --context "${machine}" build --quiet -t "${DOCKER_NAME}:${TAG}" .
+        echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine (${machine_ip})"
+        docker --host "ssh://root@${machine_ip}" build --quiet -t "${DOCKER_NAME}:${TAG}" .
 
-        if docker --context "${machine}" container inspect ${DOCKER_NAME} &>/dev/null
+        if docker --host "ssh://root@${machine_ip}" container inspect ${DOCKER_NAME} &>/dev/null
         then
             echo "[INFO] Removing container '${DOCKER_NAME}:${TAG}'"
-            docker --context "${machine}" rm "${DOCKER_NAME}"
+            docker --host "ssh://root@${machine_ip}" rm --force "${DOCKER_NAME}"
         fi
 
         echo "[INFO] Creating container '${DOCKER_NAME}:${TAG}'"
-        docker --context "${machine}" run --detach --restart always --name ${DOCKER_NAME} --publish published=${DOCKER_PORT},target=4000,mode=host ${DOCKER_NAME}:${TAG}
+        docker --host "ssh://root@${machine_ip}" run --detach --restart always --name ${DOCKER_NAME} --publish published=${DOCKER_PORT},target=4000,mode=host ${DOCKER_NAME}:${TAG}
 
         echo "[INFO] Garbage-collecting Docker"
-        docker --context "${machine}" system prune --all --force
+        docker --host "ssh://root@${machine_ip}" system prune --force
 
         echo "[INFO] Finished deploying '${DOCKER_NAME}:${TAG}' on $machine"
     )&
