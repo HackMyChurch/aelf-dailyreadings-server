3893f0fe141f217b29a74454c3f2fd4c0b1c4cf1
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2024-05-07 23:00:35 +0200

    Enable Datadog APM & tracing
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,23 +16,34 @@ unzip -o "${DICT_ARCHIVE}" "fr-classique.aff" "fr-classique.dic"
 case $1 in
     prod)
         echo "[INFO] Will deploy in *prod* mode"
+        APP_ENV="prod"
         HOSTS="prod.api.app.epitre.co"
         DOCKER_NAME="aelf-api"
         DOCKER_PORT=4001
         ;;
     *)
         echo "[INFO] Will deploy in *BETA* mode (to deploy in prod, use --> $0 prod)"
+        APP_ENV="beta"
         HOSTS="beta.api.app.epitre.co"
         DOCKER_NAME="aelf-api-beta"
         DOCKER_PORT=4002
         ;;
 esac
 
+# Build Docker run command
+DOCKER_RUN_ARGS=""
+DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --detach --restart always"
+DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --publish published=${DOCKER_PORT},target=4000,mode=host"
+DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_PROFILING_ENABLED=true"
+DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_ENV=${APP_ENV}"
+DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_SERVICE=aelf-api"
+
 # Build and deploy
 TAG=$(date +"%Y-%m-%d-%H-%M-%S")
 for machine_ip in $(dig +short "${HOSTS}")
 do
     machine="$(dig +short -x "${machine_ip}")"
+
     (
         echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine (${machine_ip})"
         docker --host "ssh://root@${machine_ip}" build --quiet -t "${DOCKER_NAME}:${TAG}" .
@@ -44,7 +55,7 @@ do
         fi
 
         echo "[INFO] Creating container '${DOCKER_NAME}:${TAG}'"
-        docker --host "ssh://root@${machine_ip}" run --detach --restart always --name ${DOCKER_NAME} --publish published=${DOCKER_PORT},target=4000,mode=host ${DOCKER_NAME}:${TAG}
+        docker --host "ssh://root@${machine_ip}" run ${DOCKER_RUN_ARGS} --name ${DOCKER_NAME} ${DOCKER_NAME}:${TAG}
 
         echo "[INFO] Garbage-collecting Docker"
         docker --host "ssh://root@${machine_ip}" system prune --force
