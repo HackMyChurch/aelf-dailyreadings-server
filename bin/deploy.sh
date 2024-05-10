f3c0754a845e1313411341ea774233b0df6995a5
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2024-05-10 16:32:08 +0200

    Refactor deploy script to support retries and target filter
    The retries are needed to help improve deployment consistency. Before
    this change, one-off errors on one of the machine could cause the deploy
    to only reach a subset of the machines. This is still not perfect as the
    script still reports a success in case of errors, but issue likelihood
    is now reduced.
    
    Second issue was that the script was only working on all machines, with
    not possible restriction. This is a problem when testing a configuration
    in a specific machine before applying to all 3 machines. This commit
    adds a '--limit' flag inspired by Ansible for this purpose.
    
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,55 @@
 
 set -e
 
+#
+# Parse arguments
+#
+
+POSITIONAL_ARGS=()
+
+LIMIT="*"
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        -l|--limit)
+            LIMIT="$2"
+            shift
+            ;;
+        -*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1")
+            ;;
+    esac
+    shift
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+APP_ENV=${1:-beta}
+
+# Determine environment
+case "${APP_ENV}" in
+    prod)
+        echo "[INFO] Will deploy in *prod* mode"
+        HOSTS="prod.api.app.epitre.co"
+        DOCKER_NAME="aelf-api"
+        DOCKER_PORT=4001
+        ;;
+    *)
+        echo "[INFO] Will deploy in *BETA* mode (to deploy in prod, use --> $0 prod)"
+        HOSTS="beta.api.app.epitre.co"
+        DOCKER_NAME="aelf-api-beta"
+        DOCKER_PORT=4002
+        ;;
+esac
+
+#
+# Fetch the dictionnaries
+#
+
 # Make sure we have the dicts
 DICT_VERSION="6.3"
 DICT_ARCHIVE="hunspell-french-dictionaries-v${DICT_VERSION}.zip"
@@ -12,23 +61,9 @@ fi
 
 unzip -o "${DICT_ARCHIVE}" "fr-classique.aff" "fr-classique.dic"
 
-# Determine environment
-case $1 in
-    prod)
-        echo "[INFO] Will deploy in *prod* mode"
-        APP_ENV="prod"
-        HOSTS="prod.api.app.epitre.co"
-        DOCKER_NAME="aelf-api"
-        DOCKER_PORT=4001
-        ;;
-    *)
-        echo "[INFO] Will deploy in *BETA* mode (to deploy in prod, use --> $0 prod)"
-        APP_ENV="beta"
-        HOSTS="beta.api.app.epitre.co"
-        DOCKER_NAME="aelf-api-beta"
-        DOCKER_PORT=4002
-        ;;
-esac
+#
+# Build and start the application
+#
 
 # Build Docker run command
 DOCKER_RUN_ARGS=""
@@ -38,29 +73,51 @@ DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_PROFILING_ENABLED=true"
 DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_ENV=${APP_ENV}"
 DOCKER_RUN_ARGS="${DOCKER_RUN_ARGS} --env DD_SERVICE=aelf-api"
 
+function deploy_on() {
+    local machine="$1"
+
+    echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine"
+    docker --host "ssh://root@${machine}" build --quiet -t "${DOCKER_NAME}:${TAG}" .
+
+    if docker --host "ssh://root@${machine}" container inspect ${DOCKER_NAME} &>/dev/null
+    then
+        echo "[INFO] Removing container '${DOCKER_NAME}:${TAG}'"
+        docker --host "ssh://root@${machine}" rm --force "${DOCKER_NAME}"
+    fi
+
+    echo "[INFO] Creating container '${DOCKER_NAME}:${TAG}'"
+    docker --host "ssh://root@${machine}" run ${DOCKER_RUN_ARGS} --name ${DOCKER_NAME} ${DOCKER_NAME}:${TAG}
+
+    echo "[INFO] Garbage-collecting Docker"
+    docker --host "ssh://root@${machine}" system prune --force
+
+    echo "[INFO] Finished deploying '${DOCKER_NAME}:${TAG}' on $machine"
+}
+
 # Build and deploy
 TAG=$(date +"%Y-%m-%d-%H-%M-%S")
 for machine_ip in $(dig +short "${HOSTS}")
 do
     machine="$(dig +short -x "${machine_ip}")"
 
+    if [[ "${machine}" != ${LIMIT} ]]
+    then
+        echo "[INFO] Skipping ${machine} (not included by "${LIMIT}" pattern)"
+        continue
+    fi
+
     (
-        echo "[INFO] Starting build of '${DOCKER_NAME}:${TAG}' on $machine (${machine_ip})"
-        docker --host "ssh://root@${machine_ip}" build --quiet -t "${DOCKER_NAME}:${TAG}" .
+        # Attempt deployment up to 3 times
+        for i in {1..3}
+        do
+            if deploy_on "${machine}"
+            then
+                exit 0
+            fi
+        done
 
-        if docker --host "ssh://root@${machine_ip}" container inspect ${DOCKER_NAME} &>/dev/null
-        then
-            echo "[INFO] Removing container '${DOCKER_NAME}:${TAG}'"
-            docker --host "ssh://root@${machine_ip}" rm --force "${DOCKER_NAME}"
-        fi
-
-        echo "[INFO] Creating container '${DOCKER_NAME}:${TAG}'"
-        docker --host "ssh://root@${machine_ip}" run ${DOCKER_RUN_ARGS} --name ${DOCKER_NAME} ${DOCKER_NAME}:${TAG}
-
-        echo "[INFO] Garbage-collecting Docker"
-        docker --host "ssh://root@${machine_ip}" system prune --force
-
-        echo "[INFO] Finished deploying '${DOCKER_NAME}:${TAG}' on $machine"
+        echo "[ERROR] Failed to deploy on ${machine}"
+        exit 1
     )&
 done
 
